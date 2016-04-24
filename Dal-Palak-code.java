8157aea9c01aba37f42e682dbcf84e6069e32eb9
Pooshan Vyas <pooshan.vyas@gmail.com>
2016-04-23 19:29:13 -0700

    Code added = Fun added

---
+++ Dal-Palak-code.java
@@ -0,0 +1,37 @@
+public class Main {
+// recipe "methods" for Daal-Paalak
+while(stove==isOn() && oil==isHeated()){
+add(rayi);
+add(jeera);
+wait(30);
+Ingredients.addOnions(0.5);
+while(onion!=brown){
+stirr=true;
+}
+stirr=false;
+Ingredients.addTomato(1);
+Ingredients.addChillies(green,2);
+while(tomato!=soft && chillies!=fried){
+stirr=true;
+}
+stirr=false;
+Masala.addTurmeric(1);
+Masala.addRedChilliPowder(2); // or in my case 3
+stirr=true;
+Ingredients.addGroundNuts("swaad anusaar");
+Ingredients.addSalt("swaad anusaar");
+Ingredients.addBoiledVeggies();
+}
+public static void boilVeggies(){
+int daal = 2;
+int paalak = 2;
+int timer = 400; 
+double water = Utensils.getCookerSize() / 2;
+while( stove==isOn() && whistle♥){
+wait(timer);
+whistle++;
+timer=timer-100;
+}
+wait(200);
+}
+}
