d98d9c5adf82506ecc4ecfa5d8764061b03070b6
Pooshan Vyas <pooshan.vyas@gmail.com>
2016-04-26 00:39:24 -0700

    Update and rename Pyramid.py to Sierpinski Triangle.py

--- Sierpinski Triangle.py
+++ Sierpinski Triangle.py
@@ -11,8 +11,7 @@ def drawTriangle(points,color,myTurtle):
     myTurtle.goto(points[0][0],points[0][1])
     myTurtle.end_fill()
 
-def function():
-   pass getMid(p1,p2):
+def getMid(p1,p2):
     return ( (p1[0]+p2[0]) / 2, (p1[1] + p2[1]) / 2)
 
 def sierpinski(points,degree,myTurtle):
